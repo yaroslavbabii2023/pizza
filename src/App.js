41d05301fcacb4d0425d289949e7678aaa7f8193
Yaroslav <yaroslav.babii@ukr.net>
2023-08-08 21:40:43 +0300

    created in the App.js fetch response

--- src/App.js
+++ src/App.js
@@ -3,10 +3,19 @@ import Header from "./components/Header";
 import Categories from "./components/Categories";
 import Sort from "./components/Sort";
 import PizzaBlock from "./components/PizzaBlock";
-import pizzas from "./assets/pizza.json"
+import React from "react";
 
 
 function App() {
+    const [items, setItems] = React.useState([])
+    React.useEffect(() => {
+        fetch('https://64d26e02f8d60b1743620bc0.mockapi.io/items')
+            .then(response => response.json())
+            .then((json) => {
+                setItems(json)
+            })
+    }, [])
+
     return (
         <div className="wrapper">
             <Header/>
@@ -18,7 +27,7 @@ function App() {
                     </div>
                     <h2 className="content__title">Все пиццы</h2>
                     <div className="content__items">
-                        {pizzas.map(obj =>
+                        {items.map(obj =>
                             <PizzaBlock key={obj.id}{...obj}/>)}
                     </div>
                 </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
+import logo from './../assets/img/pizza-logo.svg'
 function Header() {
     return (
         <div className="header">
             <div className="container">
                 <div className="header__logo">
-                    <img width="38" src="./img/pizza-logo.svg" alt="Pizza logo"/>
+                    <img width="38" src={logo} alt="Pizza logo"/>
                     <div>
                         <h1>React Pizza</h1>
                         <p>самая вкусная пицца во вселенной</p>
