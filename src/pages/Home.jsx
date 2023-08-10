7f47bf1daf3031c8b6ad6afd2643a3246f053e83
Yaroslav <yaroslav.babii@ukr.net>
2023-08-10 09:10:15 +0300

    added BrowserRouter and use Routes, Route

---
+++ src/pages/Home.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+import Categories from "../components/Categories";
+import Sort from "../components/Sort";
+import Skeleton from "../components/PizzaBlock/Skeleton";
+import PizzaBlock from "../components/PizzaBlock";
+
+
+const Home = () => {
+    const [items, setItems] = React.useState([])
+    const [isLoading, setIsLoading] = React.useState(true)
+
+    React.useEffect(() => {
+        fetch('https://64d26e02f8d60b1743620bc0.mockapi.io/items')
+            .then(response => response.json())
+            .then((json) => {
+                setItems(json)
+                setIsLoading(false)
+            })
+    }, [])
+    return(
+        <>
+            <div className="content__top">
+                <Categories/>
+                <Sort/>
+            </div>
+            <h2 className="content__title">Все пиццы</h2>
+            <div className="content__items">
+                {isLoading
+                    ? [...new Array(6)].map((_, index) => <Skeleton key={index}/>)
+                    : items.map(obj => <PizzaBlock key={obj.id} {...obj}/>)
+                }
+            </div>
+        </>
+    )
+}
+
+export default Home
