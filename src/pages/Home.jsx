6a477d3e4a9c177c8901a26c8664f81848e8cf72
Yaroslav <yaroslav.babii@ukr.net>
2023-08-16 12:55:27 +0300

    create Context in the App Component and React.useContext Search and Home Component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,44 +5,34 @@ import Sort from "../components/Sort";
 import Skeleton from "../components/PizzaBlock/Skeleton";
 import PizzaBlock from "../components/PizzaBlock";
 import Pagination from "../components/Pagination";
+import {SearchContext} from "../App";
 
+const Home = () => {
+    const {searchValue} = React.useContext(SearchContext)
 
-const Home = ({searchValue}) => {
     const [items, setItems] = React.useState([])
     const [isLoading, setIsLoading] = React.useState(true)
     const [categoryId, setCategoryId] = React.useState(0)
     const [currentPage, setCurrentPage] = React.useState(1)
-    const [sortType, setSortType] = React.useState({
-        name: 'популярности', sortProperty: 'rating'
-    })
+    const [sortType, setSortType] = React.useState({name: 'популярности', sortProperty: 'rating'})
 
     React.useEffect(() => {
         setIsLoading(true)
-
         const order = sortType.sortProperty.includes('-') ? 'asc' : 'desc';
         const sortBy = sortType.sortProperty.replace('-', '')
         const category = categoryId > 0 ? `category=${categoryId}` : ''
         const search = searchValue ? `&search=${searchValue}` : ''
-
         fetch(`https://64d26e02f8d60b1743620bc0.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`)
             .then(response => response.json())
             .then((json) => {
                 setItems(json)
                 setIsLoading(false)
             })
-
         window.scrollTo(0, 0)
     }, [categoryId, sortType, searchValue, currentPage])
 
     const skeletons = [...new Array(6)].map((_, index) => <Skeleton key={index}/>)
-    const pizzas = items
-        //     .filter((obj) => {
-        //     if (obj.title.toLowerCase().includes(searchValue.toLowerCase())) {
-        //         return true
-        //     }
-        //     return false
-        // })
-        .map(obj => <PizzaBlock key={obj.id} {...obj}/>)
+    const pizzas = items.map(obj => <PizzaBlock key={obj.id} {...obj}/>)
 
 
     return (
