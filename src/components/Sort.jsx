ee8e62cef12b6c78f5a6b704af080baa44bdb0f1
Yaroslav <yaroslav.babii@ukr.net>
2023-08-20 16:53:14 +0300

    added useEffect in the Sort.jsx for hide/show popup sort

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -15,14 +15,26 @@ function Sort() {
     const dispatch = useDispatch()
     const sort = useSelector(state => state.filter.sort)
     const [open, setOpen] = React.useState(false)
+    const sortRef = React.useRef();
 
     const onChangeList = (obj) => {
         dispatch(setSort(obj))
         setOpen(false)
     }
 
+    React.useEffect(() => {
+        const handleClickOutside = event => {
+            if (!event.composedPath().includes(sortRef.current)) {
+                setOpen(false)
+            }
+        }
+        document.body.addEventListener('click', handleClickOutside)
+
+        return () => document.body.removeEventListener('click', handleClickOutside)
+    }, [])
+
     return (
-        <div className="sort">
+        <div ref={sortRef} className="sort">
             <div className="sort__label">
                 <svg
                     width="10"
