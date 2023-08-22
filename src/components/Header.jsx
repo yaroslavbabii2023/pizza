8d84f0dc6bd942bb17bdaae904ab2c1fb8dc122b
Yaroslav <yaroslav.babii@ukr.net>
2023-08-22 15:11:04 +0300

    added CartEmpty.jsx, CartItems.jsx, cartSlice.js and modified Cart.jsx, Header.jsx, PizzaBlock and store

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,11 @@
 import logo from './../assets/img/pizza-logo.svg'
 import {Link} from "react-router-dom";
 import Search from "./Serch";
-function Header () {
+import {useSelector} from 'react-redux'
+
+function Header() {
+    const {items, totalPrice} = useSelector(state => state.cart)
+    const totalCount = items.reduce((sum, item) => sum + item.count, 0)
     return (
         <div className="header">
             <div className="container">
@@ -17,7 +21,7 @@ function Header () {
                 <Search/>
                 <div className="header__cart">
                     <Link to="/cart" className="button button--cart">
-                        <span>520 ₽</span>
+                        <span>{totalPrice} ua</span>
                         <div className="button__delimiter"></div>
                         <svg
                             width="18"
@@ -48,7 +52,7 @@ function Header () {
                                 strokeLinejoin="round"
                             />
                         </svg>
-                        <span>3</span>
+                        <span>{totalCount}</span>
                     </Link>
                 </div>
             </div>
---
+++ src/redux/slices/cartSlice.js
@@ -0,0 +1,42 @@
+import {createSlice} from "@reduxjs/toolkit";
+
+const initialState = {
+    totalPrice: 0,
+    items: [],
+};
+
+const cartSlice = createSlice({
+    name: 'cart',
+    initialState,
+    reducers: {
+        addItems(state, action) {
+            const findItems = state.items.find(obj => obj.id === action.payload.id)
+            if (findItems) {
+                findItems.count++
+            } else {
+                state.items.push({
+                    ...action.payload,
+                    count: 1
+                })
+            }
+            state.totalPrice = state.items.reduce((sum, obj) => {
+                return (obj.price * obj.count) + sum
+            }, 0)
+        },
+        minusItem(state, action) {
+            const findItems = state.items.find(obj => obj.id === action.payload)
+            if (findItems) {
+                findItems.count--
+            }
+        },
+        removeItems(state, action) {
+            state.items = state.items.filter(obj => obj.id !== action.payload)
+        },
+        clearItems(state) {
+            state.items = []
+            state.totalPrice = 0
+        },
+    }
+})
+export const {addItems, removeItems, clearItems, minusItem} = cartSlice.actions
+export default cartSlice.reducer
